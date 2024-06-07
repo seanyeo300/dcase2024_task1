dd38be575ebbafc8540c8b7f45ddec42415394c8
seanyeo300 <seanyeo300@gmail.com>
2024-06-07 13:46:09 +0800

    Update get_logits.py

--- get_logits.py
+++ get_logits.py
@@ -154,7 +154,7 @@ class PLModule(pl.LightningModule):
         results = {'loss': samples_loss.mean(), "n_correct": n_correct,
                    "n_pred": torch.as_tensor(len(labels), device=self.device)}
 
-        # log metric per device and scene
+        '''# log metric per device and scene
         for d in self.device_ids:
             results["devloss." + d] = torch.as_tensor(0., device=self.device)
             results["devcnt." + d] = torch.as_tensor(0., device=self.device)
@@ -173,7 +173,7 @@ class PLModule(pl.LightningModule):
             results["lbln_correct." + self.label_ids[l]] = \
                 results["lbln_correct." + self.label_ids[l]] + n_correct_per_sample[i]
             results["lblcnt." + self.label_ids[l]] = results["lblcnt." + self.label_ids[l]] + 1
-        results = {k: v.cpu() for k, v in results.items()}
+        results = {k: v.cpu() for k, v in results.items()}'''
         self.validation_step_outputs.append(results)
 
     def on_validation_epoch_end(self):
@@ -190,7 +190,7 @@ class PLModule(pl.LightningModule):
 
         logs = {'acc': acc, 'loss': avg_loss}
 
-        # log metric per device and scene
+        '''# log metric per device and scene
         for d in self.device_ids:
             dev_loss = outputs["devloss." + d].sum()
             dev_cnt = outputs["devcnt." + d].sum()
@@ -216,7 +216,7 @@ class PLModule(pl.LightningModule):
             logs["cnt." + l] = lbl_cnt
 
         logs["macro_avg_acc"] = torch.mean(torch.stack([logs["acc." + l] for l in self.label_ids]))
-        # prefix with 'val' for logging
+        # prefix with 'val' for logging'''
         self.log_dict({"val/" + k: logs[k] for k in logs})
         self.validation_step_outputs.clear()
 
@@ -471,7 +471,7 @@ if __name__ == '__main__':
 
     # general
     parser.add_argument('--project_name', type=str, default="DCASE24_Task1")
-    parser.add_argument('--experiment_name', type=str, default="Baseline_DSP_sub5_eval_16BC")
+    parser.add_argument('--experiment_name', type=str, default="Baseline_DSP_sub5_eval_8BC")
     parser.add_argument('--num_workers', type=int, default=0)  # number of workers for dataloaders
     parser.add_argument('--precision', type=str, default="32")
 
@@ -482,7 +482,7 @@ if __name__ == '__main__':
     # dataset
     # subset in {100, 50, 25, 10, 5}
     parser.add_argument('--orig_sample_rate', type=int, default=44100)
-    parser.add_argument('--subset', type=int, default=100)
+    parser.add_argument('--subset', type=int, default=5)
 
     # model
     parser.add_argument('--n_classes', type=int, default=10)  # classification model with 'n_classes' output neurons
@@ -502,7 +502,7 @@ if __name__ == '__main__':
 
     # peak learning rate (in cosinge schedule)
     parser.add_argument('--lr', type=float, default=0.005)
-    parser.add_argument('--warmup_steps', type=int, default=0)
+    parser.add_argument('--warmup_steps', type=int, default=100)
 
     # preprocessing
     parser.add_argument('--sample_rate', type=int, default=44100)
