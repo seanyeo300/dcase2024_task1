b750af5e5d8cf04fc710f6b9669f19866546814b
seanyeo300 <seanyeo300@gmail.com>
2024-06-12 21:10:56 +0800

    upload ensemble logits and split training_KD into two gpu

--- multi_run_eval.py
+++ multi_run_eval.py
@@ -11,18 +11,18 @@ def run_multiple_scripts(scripts_with_args):
 if __name__ == "__main__":
     # Example: Replace these with your actual script names and arguments
     scripts_to_run = [
-        ('get_logits.py', ['--evaluate', '--ckpt_id', 'pw3jremw']),
-        ('get_logits.py', ['--evaluate', '--ckpt_id', 'baeix291']),
-        ('get_logits.py', ['--evaluate', '--ckpt_id', '7qouvmdh'])
-        # ('get_logits.py', ['--evaluate', '--ckpt_id', 'pcerkmrt'])
+        ('get_logits.py', ['--evaluate', '--ckpt_id', '3xlli7dq']),
+        ('get_logits.py', ['--evaluate', '--ckpt_id', '1e5ld4y6']),
+        ('get_logits.py', ['--evaluate', '--ckpt_id', 'gs5hm18o']),
+        ('get_logits.py', ['--evaluate', '--ckpt_id', 'nmwun6cs'])
         # ('get_logits.py', ['--evaluate', '--ckpt_id', 'duy4puj8'])
     ]
     run_multiple_scripts(scripts_to_run)
 #50%
-["3xlli7dq", "wpu6shc2", "8cbb6x4v"]
+["3xlli7dq"]#, "wpu6shc2", "8cbb6x4v"]
 #25%
-["b9ooz0ks", "dksmk72n", "29rio0lo"]    
+["b9ooz0ks", "dksmk72n", "29rio0lo"]    #done  
 #10%
-['pw3jremw', 'baeix291', '7qouvmdh']
+['pw3jremw', 'baeix291', '7qouvmdh']    #done
 #5%
 ["nmwun6cs", "1e5ld4y6", "gs5hm18o"]
