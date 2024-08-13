bfd0556b8a08323608876557c8a766aa58526c57
seanyeo300 <seanyeo300@gmail.com>
2024-08-13 13:58:15 +0800

    Update run_training_cochl_pretrain_mel.py

--- run_training_cochl_pretrain_mel.py
+++ run_training_cochl_pretrain_mel.py
@@ -11,7 +11,7 @@ import json
 import torch.utils.data as data
 
 from torch.autograd import Variable
-from dataset.cochl_ntu_teacher_10s import ntu_get_training_set_dir, ntu_get_test_set, ntu_get_eval_set, open_h5, close_h5
+from dataset.cochl_ntu_teacher_10s import ntu_get_training_set_dir, ntu_get_test_set, ntu_get_eval_set, open_h5, close_h5, ntu_get_test_set_cochl
 from helpers.init import worker_init_fn
 from models.baseline import get_model
 from helpers.utils import mixstyle, mixup_data
@@ -256,29 +256,29 @@ class PLModule(pl.LightningModule):
         n_correct_per_sample = (preds == labels)
         n_correct = n_correct_per_sample.sum()
 
-        dev_names = [d.rsplit("-", 1)[1][:-4] for d in files]
+        # dev_names = [d.rsplit("-", 1)[1][:-4] for d in files]
         results = {'loss': samples_loss.mean(), "n_correct": n_correct,
                    "n_pred": torch.as_tensor(len(labels), device=self.device)}
 
-        # log metric per device and scene
-        for d in self.device_ids:
-            results["devloss." + d] = torch.as_tensor(0., device=self.device)
-            results["devcnt." + d] = torch.as_tensor(0., device=self.device)
-            results["devn_correct." + d] = torch.as_tensor(0., device=self.device)
-        for i, d in enumerate(dev_names):
-            results["devloss." + d] = results["devloss." + d] + samples_loss[i]
-            results["devn_correct." + d] = results["devn_correct." + d] + n_correct_per_sample[i]
-            results["devcnt." + d] = results["devcnt." + d] + 1
+        # # log metric per device and scene
+        # for d in self.device_ids:
+        #     results["devloss." + d] = torch.as_tensor(0., device=self.device)
+        #     results["devcnt." + d] = torch.as_tensor(0., device=self.device)
+        #     results["devn_correct." + d] = torch.as_tensor(0., device=self.device)
+        # for i, d in enumerate(dev_names):
+        #     results["devloss." + d] = results["devloss." + d] + samples_loss[i]
+        #     results["devn_correct." + d] = results["devn_correct." + d] + n_correct_per_sample[i]
+        #     results["devcnt." + d] = results["devcnt." + d] + 1
 
-        for l in self.label_ids:
-            results["lblloss." + l] = torch.as_tensor(0., device=self.device)
-            results["lblcnt." + l] = torch.as_tensor(0., device=self.device)
-            results["lbln_correct." + l] = torch.as_tensor(0., device=self.device)
-        for i, l in enumerate(labels):
-            results["lblloss." + self.label_ids[l]] = results["lblloss." + self.label_ids[l]] + samples_loss[i]
-            results["lbln_correct." + self.label_ids[l]] = \
-                results["lbln_correct." + self.label_ids[l]] + n_correct_per_sample[i]
-            results["lblcnt." + self.label_ids[l]] = results["lblcnt." + self.label_ids[l]] + 1
+        # for l in self.label_ids:
+        #     results["lblloss." + l] = torch.as_tensor(0., device=self.device)
+        #     results["lblcnt." + l] = torch.as_tensor(0., device=self.device)
+        #     results["lbln_correct." + l] = torch.as_tensor(0., device=self.device)
+        # for i, l in enumerate(labels):
+        #     results["lblloss." + self.label_ids[l]] = results["lblloss." + self.label_ids[l]] + samples_loss[i]
+        #     results["lbln_correct." + self.label_ids[l]] = \
+        #         results["lbln_correct." + self.label_ids[l]] + n_correct_per_sample[i]
+        #     results["lblcnt." + self.label_ids[l]] = results["lblcnt." + self.label_ids[l]] + 1
         self.test_step_outputs.append(results)
 
     def on_test_epoch_end(self):
@@ -296,21 +296,21 @@ class PLModule(pl.LightningModule):
         logs = {'acc': acc, 'loss': avg_loss}
 
         # log metric per device and scene
-        for d in self.device_ids:
-            dev_loss = outputs["devloss." + d].sum()
-            dev_cnt = outputs["devcnt." + d].sum()
-            dev_corrct = outputs["devn_correct." + d].sum()
-            logs["loss." + d] = dev_loss / dev_cnt
-            logs["acc." + d] = dev_corrct / dev_cnt
-            logs["cnt." + d] = dev_cnt
-            # device groups
-            logs["acc." + self.device_groups[d]] = logs.get("acc." + self.device_groups[d], 0.) + dev_corrct
-            logs["count." + self.device_groups[d]] = logs.get("count." + self.device_groups[d], 0.) + dev_cnt
-            logs["lloss." + self.device_groups[d]] = logs.get("lloss." + self.device_groups[d], 0.) + dev_loss
+        # for d in self.device_ids:
+        #     dev_loss = outputs["devloss." + d].sum()
+        #     dev_cnt = outputs["devcnt." + d].sum()
+        #     dev_corrct = outputs["devn_correct." + d].sum()
+        #     logs["loss." + d] = dev_loss / dev_cnt
+        #     logs["acc." + d] = dev_corrct / dev_cnt
+        #     logs["cnt." + d] = dev_cnt
+        #     # device groups
+        #     logs["acc." + self.device_groups[d]] = logs.get("acc." + self.device_groups[d], 0.) + dev_corrct
+        #     logs["count." + self.device_groups[d]] = logs.get("count." + self.device_groups[d], 0.) + dev_cnt
+        #     logs["lloss." + self.device_groups[d]] = logs.get("lloss." + self.device_groups[d], 0.) + dev_loss
 
-        for d in set(self.device_groups.values()):
-            logs["acc." + d] = logs["acc." + d] / logs["count." + d]
-            logs["lloss." + d] = logs["lloss." + d] / logs["count." + d]
+        # for d in set(self.device_groups.values()):
+        #     logs["acc." + d] = logs["acc." + d] / logs["count." + d]
+        #     logs["lloss." + d] = logs["lloss." + d] / logs["count." + d]
 
         for l in self.label_ids:
             lbl_loss = outputs["lblloss." + l].sum()
@@ -365,13 +365,18 @@ def train(config):
                           shuffle=True
                           )
 
-    test_dl = DataLoader(dataset=ntu_get_test_set(hf_in),
+    val_dl = DataLoader(dataset=ntu_get_test_set(hf_in),
                          worker_init_fn=worker_init_fn,
                          num_workers=config.num_workers,
                          batch_size=config.batch_size,
                          pin_memory=True,
                          )
-    
+    test_dl = DataLoader(dataset=ntu_get_test_set_cochl(hf_in),
+                         worker_init_fn=worker_init_fn,
+                         num_workers=config.num_workers,
+                         batch_size=config.batch_size,
+                         pin_memory=True,
+                         )
 
 
     # create pytorch lightening module
@@ -396,7 +401,7 @@ def train(config):
                          callbacks=[pl.callbacks.ModelCheckpoint(save_last=True, monitor = "val/loss",save_top_k=1)]
                          )
     # start training and validation for the specified number of epochs
-    trainer.fit(pl_module, train_dl, test_dl)
+    trainer.fit(pl_module, train_dl, val_dl)
     trainer.test(ckpt_path='best', dataloaders=test_dl)
 
     # close file pointer to h5 file 
@@ -501,7 +506,7 @@ if __name__ == '__main__':
     # general
     # parser.add_argument('--project_name', type=str, default="DCASE24_Task1")
     parser.add_argument('--project_name', type=str, default="NTU24_ASC")
-    parser.add_argument('--experiment_name', type=str, default="NTU_cochlpretrain_DSP_441K_DIR_FMS_32_channel_10s_h5")
+    parser.add_argument('--experiment_name', type=str, default="NTU_cochlpretrain_test")
     parser.add_argument('--num_workers', type=int, default=0)  # number of workers for dataloaders
     parser.add_argument('--precision', type=str, default="32")
 
@@ -524,7 +529,7 @@ if __name__ == '__main__':
     parser.add_argument('--expansion_rate', type=float, default=2.1)
 
     # training
-    parser.add_argument('--n_epochs', type=int, default=150)
+    parser.add_argument('--n_epochs', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=256)
     parser.add_argument('--mixstyle_p', type=float, default=0.4)  # frequency mixstyle
     parser.add_argument('--mixstyle_alpha', type=float, default=0.3)
