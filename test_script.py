6bd22596e71c7403a3a17016895b77035552cb96
seanyeo300 <seanyeo300@gmail.com>
2024-08-15 23:49:37 +0800

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -29,15 +29,48 @@ import pandas as pd
 
 from argparse import Namespace
 import h5py
-from tqdm import tqdm 
+from tqdm import tqdm
+from models.mel import AugmentMelSTFT 
 # import glob
 
-args = Namespace(project_name='DCASE24_Task1_Protonet', 
-                    experiment_name='Protonet-NTU', 
+# args = Namespace(project_name='DCASE24_BCBL', 
+#                     experiment_name='Protonet-NTU', 
+#                     num_workers=0, precision='32', 
+#                     evaluate=False, ckpt_id='057o1jd1', 
+#                     orig_sample_rate=44100, 
+#                     subset=5, n_classes=10, 
+#                     in_channels=1, 
+#                     base_channels=20, 
+#                     channels_multiplier=1.5, 
+#                     expansion_rate=2, 
+#                     n_epochs=1, 
+#                     batch_size=256, 
+#                     mixstyle_p=0.4, 
+#                     mixstyle_alpha=0.3, 
+#                     weight_decay=0.00001, 
+#                     roll_sec=0, lr=0.01, 
+#                     dir_p = 0.6,
+#                     warmup_steps=20, 
+#                     sample_rate=44100, 
+#                     window_length=3072, 
+#                     hop_length=500, 
+#                     n_fft=4096, 
+#                     n_mels=256, 
+#                     freqm=48, 
+#                     timem=0, 
+#                     f_min=0, 
+#                     f_max=None,
+#                     way=10, 
+#                     shot=5, 
+#                     query=10, 
+#                     episode=1000)
+
+args = Namespace(project_name='DCASE24_Task1_PaSST', 
+                    experiment_name='PaSST-NTU', 
                     num_workers=0, precision='32', 
                     evaluate=False, ckpt_id='057o1jd1', 
-                    orig_sample_rate=44100, 
-                    subset=5, n_classes=10, 
+                    orig_sample_rate=44100,
+                    subset=5, n_classes=13, 
                     in_channels=1, 
                     base_channels=20, 
                     channels_multiplier=1.5, 
@@ -49,29 +82,34 @@ args = Namespace(project_name='DCASE24_Task1_Protonet',
                     weight_decay=0.00001, 
                     roll_sec=0, lr=0.01, 
                     dir_p = 0.6,
-                    warmup_steps=20, 
+                    warmup_steps=20,##################### Start Here #####################
+                    resample_rate = 44100,
                     sample_rate=44100, 
-                    window_length=3072, 
-                    hop_length=500, 
-                    n_fft=4096, 
-                    n_mels=256, 
-                    freqm=48, 
-                    timem=0, 
+                    window_length=800, 
+                    hop_length=320, 
+                    n_fft=1024, 
+                    n_mels=128,
+                    freqm=0, 
+                    timem=0, ##################### Done ######################## 
                     f_min=0, 
                     f_max=None,
+                    fmin_aug_range=1,
+                    fmax_aug_range=1000,
                     way=10, 
                     shot=5, 
                     query=10, 
                     episode=1000)
-
 config = args
 # meta_csv = r"F:\DCASE\2024\Datasets\TAU-urban-acoustic-scenes-2022-mobile-development\meta.csv"
-meta_csv = r"F:\CochlScene\meta.csv"
+# meta_csv = r"F:\CochlScene\meta.csv"
+meta_csv = r"D:\Sean\CochlScene\meta.csv"
 # train_files_csv = r"F:\DCASE\2024\Datasets\TAU-urban-acoustic-scenes-2022-mobile-development\split100.csv"
-train_files_csv = r"F:\Github\dcase2024_task1\split_setup\splitcochl.csv"
+# train_files_csv = r"F:\Github\dcase2024_task1\split_setup\splitcochl.csv"
+train_files_csv = r"D:\Sean\github\cpjku_dcase23_NTU\split_setup\splitcochl.csv"
 # eval_meta_csv = 'c:/Dataset/eval_dataset_2024/meta.csv'
 # dataset_dir = r"F:\DCASE\2024\Datasets\TAU-urban-acoustic-scenes-2022-mobile-development"
-dataset_dir = r"F:\CochlScene"
+# dataset_dir = r"F:\CochlScene"
+dataset_dir = r"D:\Sean\CochlScene"
 # eval_dataset_dir = 'c:/Dataset/eva_dataset_2024/'
 # eval_csv = r"F:\DCASE\2024\Datasets\TAU-urban-acoustic-scenes-2024-mobile-evaluation\evaluation_setup\fold1_test.csv"
 eval_csv = r"F:\Github\dcase2024_task1\split_setup\val_cochl.csv"
@@ -100,6 +138,19 @@ mel = torchaudio.transforms.MelSpectrogram(
     f_min=config.f_min,
     f_max=config.f_max
     )
+mel_passt = AugmentMelSTFT(n_mels=config.n_mels,
+                                  sr=config.resample_rate,
+                                  win_length=config.window_length,
+                                  hopsize=config.hop_length,
+                                  n_fft=config.n_fft,
+                                  freqm=config.freqm,
+                                  timem=config.timem,
+                                  fmin=config.f_min,
+                                  fmax=config.f_max,
+                                  fmin_aug_range=config.fmin_aug_range,
+                                  fmax_aug_range=config.fmax_aug_range
+                                  )
+
 
 # # to create audio samples h5 file
 # df = pd.read_csv(meta_csv, sep="\t")
@@ -116,15 +167,15 @@ mel = torchaudio.transforms.MelSpectrogram(
 #     hf.create_dataset(output_str, data = mel_sig)    
 # hf.close()
 
-# Create mel chunked HDF5 file
-df = pd.read_csv(test_csv, sep="\t")
+# Create mel HDF5 file
+df = pd.read_csv(meta_csv, sep="\t")
 train_files = pd.read_csv(train_files_csv, sep='\t')['filename'].values.reshape(-1)
 files = df['filename'].values.reshape(-1)
 
-hf = h5py.File('h5py_cochl_test_mel_10s', 'w')
+hf = h5py.File('h5py_cochl_train_mel_10s_128bins', 'w')
 for file_idx in tqdm(range(len(files))):
     sig, _ = torchaudio.load(os.path.join(dataset_dir, files[file_idx]))
-    mel_sig = mel(sig)
+    mel_sig = mel_passt(sig)
     output_str = files[file_idx][5:-4]
     hf.create_dataset(output_str, data=mel_sig)
 hf.close()
