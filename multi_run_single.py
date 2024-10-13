4aaeee183b691306879144bcd3bff10dfdbd07ca
seanyeo300 <seanyeo300@gmail.com>
2024-10-13 15:01:53 +0800

    Update multi_run_single.py

--- multi_run_single.py
+++ multi_run_single.py
@@ -39,10 +39,11 @@ def run_multiple_scripts(script_name, base_args, ckpt_experiment_pairs, num_repe
 
 if __name__ == "__main__":
     # Define the script to run
-    script_name = 'run_training_dev_h5.py'
+    # script_name = 'run_training_dev_h5.py'
     # script_name = 'run_passt_cochl_tau_slowfast_subsets_DIR_FMS_h5.py'
+    script_name = 'run_training_DynMN_h5_PL.py'
     # Base arguments (common to all runs, except experiment name and ckpt_id)
-    base_args = ["--subset", "10", "--dir_prob", "0.6", "--mixstyle_p", "0.4"]
+    base_args = ['--gpu','[0]',"--subset", "5", "--dir_prob", "0.6", "--mixstyle_p", "0.4", "--batch_size", "48","--model_name", "dymn20_as", "--model_width", "2.0"]
     
     # List of tuples containing checkpoint IDs and their corresponding experiment names
     ckpt_experiment_pairs = [
@@ -53,15 +54,16 @@ if __name__ == "__main__":
         # ("wk50wxro", "sBCBL_SLcs_FTtau_wk50wxro_sub10_FMS_DIR_fixh5"),       #SL 1e-4 SIT
         # ("qrrag30b", "sBCBL_SLcs_FTtau_qrrag30b_sub10_FMS_DIR_fixh5"),       #SL 1e-5 SIT
         # ("7qghtor2", "sBCBL_SLcs_FTtau_7qghtor2_sub10_FMS_DIR_fixh5")        #SL 1e-6 SIT 
-        ("wk50wxro", "sBCBL_SLcs_SLtau_wk50wxro_sub10_FMS_DIR_fixh5"),       #SL 1e-4 DSIT 
+        # ("wk50wxro", "sBCBL_SLcs_SLtau_wk50wxro_sub10_FMS_DIR_fixh5"),       #SL 1e-4 DSIT 
         # ("qrrag30b", "sBCBL_SLcs_SLtau_qrrag30b_sub10_FMS_DIR_fixh5"),       #SL 1e-5 DSIT
         # ("7qghtor2", "sBCBL_SLcs_SLtau_7qghtor2_sub10_FMS_DIR_fixh5")        #SL 1e-6 DSIT
         # (None, "sBCBL_FTtau_sub10_FMS_DIR_fixh5")                              #PTau First model trained used mixup, have disabled default mixup for the next 5
+        (None, "tDynMN20_FTtau_32K_FMS_DIR_noAS_sub5_fixh5")                       #FTtau_noASpt 
 
     ] 
     
     # Number of times to repeat each experiment
-    num_repeats = 6
+    num_repeats = 2
 
     # Run the script with different checkpoint IDs and experiment names
     run_multiple_scripts(script_name, base_args, ckpt_experiment_pairs, num_repeats)
