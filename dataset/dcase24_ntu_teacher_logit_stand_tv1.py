59d2199199f6d7555f7df7851d2cf7edec62fdba
seanyeo300 <seanyeo300@gmail.com>
2024-10-30 14:48:18 +0800

    Update dcase24_ntu_teacher_logit_stand_tv1.py

--- dataset/dcase24_ntu_teacher_logit_stand_tv1.py
+++ dataset/dcase24_ntu_teacher_logit_stand_tv1.py
@@ -36,9 +36,9 @@ dataset_config = {
     # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_6_PaSST_tv3b.pt") # tv3
     # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_6_PASST_TA2.pt")  #TA2
     # "logits_file": os.path.join("predictions","ensemble", "sub5_DyMN20_TA1.pt") # TA1->BCBL
-    # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_DCASE_aug.pt") # DCASE ensemble 
-    "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_DCASE_style.pt")
-    # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL.pt")
+    # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_DCASE_aug.pt")       # DCASE ensemble 
+    # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_DCASE_style.pt")       # 3 BCBL 3 PaSST
+    "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL.pt")      # 3 BCBL 3 SIT
     #sub10
     # "logits_file": os.path.join("predictions","ensemble", "sub10_ensemble_6_PASST_tv1b.pt") # 
     # "logits_file": os.path.join("predictions","ensemble", "sub10_ensemble_6_PaSST_tv2c.pt") # tv2 
