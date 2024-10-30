5e821f90844c4d7335b4a98f525809c456f83581
seanyeo300 <seanyeo300@gmail.com>
2024-10-30 14:47:51 +0800

    Update multi_run_tv1.py

--- multi_run_tv1.py
+++ multi_run_tv1.py
@@ -67,33 +67,38 @@ if __name__ == "__main__":
     # script_name = 'run_training_DynMN_h5_KD_tv1.py'
     script_name = 'run_training_KD_logit_stand_h5_tv1.py' 
     # script_name = 'run_training_KD_teacher_logit_stand_h5_tv1.py'
+    # script_name = 'run_training_DynMN_h5_KD_logit_stand_tv1.py'
     
     # Base arguments (common to all runs, except experiment name and ckpt_id)
-    base_args = ['--gpu','[1]',"--subset", "5", "--dir_prob", "0.6", "--mixstyle_p", "0.4","--logit_stand","--kd_lambda","0.05","--temperature","3"]#, "--batch_size", "48","--pretrained","--model_name", "dymn10_as"] # this is for the KD process, does not apply to teachers!!! 
+    base_args = ['--gpu','[0]',"--subset", "5", "--dir_prob", "0.6", "--mixstyle_p", "0.4","--kd_lambda","0.07","--temperature","3","--logit_stand"]#, "--batch_size", "48","--pretrained","--model_name", "dymn10_as"] # this is for the KD process, does not apply to teachers!!! "--logit_stand"
     
     # List of tuples containing checkpoint IDs and their corresponding experiment names
     ckpt_experiment_pairs = [
-        # ("fskag87u", "NTU_KD_Var1-T_DSIT-S_FMS_DIR_sub5_fixh5"), #DSIT
-        # ("leguwmeg", "NTU_KD_Var1-T_SIT-S_FMS_DIR_sub5_fixh5"),  #SIT FMS DIR
-        # ("dbl1yun4", "NTU_KD_Var1-T_SIT-S_FMS_sub5_fixh5"),      #SIT FMS
-        # ("lm7o54or", "NTU_KD_Var1-T_SeqFT-S_FMS_DIR_sub5_fixh5"),#SeqFT
-        # ("ke771aaz", "NTU_KD_Var1-T_FTtau-S_FMS_DIR_sub10_fixh5"),#FTtau FMS DIR
-        # ("y7frm0sm", "NTU_KD_Var1-T_FTtau-S_FMS_sub5_fixh5"), #FTtau FMS
-        # ("eqov5ca2", "NTU_KD_Var1-T_FTtau-S_FMS_DIR_Mixup_sub5_fixh5"), #FTtau FMS DIR MIXUP
-        # (None, "NTU_KD_tv3b-T_32BCBL-S_FMS_DIR_stand_lmda=0.05_sub5_fixh5")          #tv1b
-        # (None, "NTU_KD_Dy10TA1-T_32BCBL-S_FMS_DIR_sub5_fixh5")          #TA1
-        # (None, "NTU_KD_tv1b-T_DyMN20-TA_FMS_DIR_sub5_fixh5")     # Dymn20 tv1b
-        # (None, "NTU_KD_DyTA1-T_32BCBL-S_FMS_DIR_sub5_fixh5")        #TA1->BCBL distillation
-        # (None, "NTU_KD_Dy20TA1-TA_Dy10TA1-TA_FMS_DIR_sub5_fixh5")    # tv1->Dymn20->Dymn10
+        # ("fskag87u", "NTU_KD_Var1-T_DSIT-S_FMS_DIR_sub5_fixh5"),                               #DSIT
+        # ("leguwmeg", "NTU_KD_Var1-T_SIT-S_FMS_DIR_sub5_fixh5"),                                #SIT FMS DIR
+        # ("dbl1yun4", "NTU_KD_Var1-T_SIT-S_FMS_sub5_fixh5"),                                    #SIT FMS
+        # ("lm7o54or", "NTU_KD_Var1-T_SeqFT-S_FMS_DIR_sub5_fixh5"),                              #SeqFT
+        # ("ke771aaz", "NTU_KD_Var1-T_FTtau-S_FMS_DIR_sub10_fixh5"),                             #FTtau FMS DIR
+        # ("y7frm0sm", "NTU_KD_Var1-T_FTtau-S_FMS_sub5_fixh5"),                                  #FTtau FMS
+        # ("eqov5ca2", "NTU_KD_Var1-T_FTtau-S_FMS_DIR_Mixup_sub5_fixh5"),                        #FTtau FMS DIR MIXUP
+        # (None, "NTU_KD_tv3b-T_32BCBL-S_FMS_DIR_stand_lmda=0.05_sub5_fixh5")                    #tv1b
+        # (None, "NTU_KD_Dy10TA1-T_32BCBL-S_FMS_DIR_sub5_fixh5")                                 #TA1
+        # (None, "NTU_KD_tv1b-T_DyMN20-TA_FMS_DIR_sub5_fixh5")                                   # Dymn20 tv1b
+        # (None, "NTU_KD_DyTA1-T_32BCBL-S_FMS_DIR_sub5_fixh5")                                   #TA1->BCBL distillation 
+        # (None, "NTU_KD_Dy20TA1-TA_Dy10TA1-TA_FMS_DIR_sub5_fixh5")                              # tv1->Dymn20->Dymn10 T=2, lmbda=0.02
+        # (None, "NTU_KD_Dy20TA1-TA_Dy10TA1-TA_FMS_DIR_T=4_lmbda=0.05_sub5_fixh5")           # tv1->Dymn20->Dymn10 logit stand, T=3, lmbda=0.05
         # (None, "NTU_KD_EnDy20TA1-T_32BCBL-S_FMS_DIR_sub5_fixh5")                               # DyMN20 TA1 Ensemble -> BCBL
-        # (None, "NTU_KD_3SIT3BCBL-T_32BCBL-S_FMS_DIR_stand_T=3_lmbda=0.05_sub5_fixh5")          # DCASE SIT BCBL logit stand
-        # (None, "NTU_KD_3SIT3BCBL-T_32BCBL-S_FMS_DIR_sub5_fixh5")                               # DCASE SIT BCBL logit stand
-        (None, "NTU_KD_3PaSST3BCBL-T_32BCBL-S_FMS_DIR_stand_T=3_lmbda=0.05_sub5_fixh5")          # DCASE ensemble logit stand
+        (None, "NTU_KD_3SIT3BCBL-T_32BCBL-S_FMS_DIR_stand_T=3_lmbda=0.07_sub5_fixh5")          # DCASE SIT BCBL logit stand, T=3, lmbda=0.05
+        # (None, "NTU_KD_3SIT3BCBL-T_32BCBL-S_FMS_DIR_T=2_lmbda=0.02_sub5_fixh5")                # DCACSE SIT BCBL NAIVE TEMP
+        # (None, "NTU_KD_3SIT3BCBL-T_32BCBL-S_FMS_DIR_T=3_lmbda=0.05_sub5_fixh5")                # DCASE SIT BCBL T=3, lmbda = 0.05
+        # (None, "NTU_KD_3PaSST3BCBL-T_32BCBL-S_FMS_DIR_stand_T=3_lmbda=0.05_sub5_fixh5")        # DCASE PaSST BCBL logit stand, T=3, lmbda=0.05
+        # (None, "NTU_KD_3PaSST3BCBL-T_32BCBL-S_FMS_DIR_T=3_lmbda=0.05_sub5_fixh5")                # DCASE PaSST BCBL T=3, lmbda=0.05
+        # (None, "NTU_KD_3PaSST3BCBL-T_32BCBL-S_FMS_DIR_T=2_lmbda=0.02_sub5_fixh5")              # DCASE PaSST BCBL T=2, lmbda=0.02
         # (None, "NTU_KD_tv1b-T_32BCBL-S_FMS_DIR_stand_lmda=0.1_sub5_fixh5") #tv1 logit stand
     ]
     
     # Number of times to repeat each experiment
-    num_repeats = 1
+    num_repeats = 2
 
     # Run the script with different checkpoint IDs and experiment names
     run_multiple_scripts(script_name, base_args, ckpt_experiment_pairs, num_repeats)
