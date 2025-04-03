1488e0aec2e3530dd0668be77279379a2861c072
seanyeo300 <seanyeo300@gmail.com>
2025-04-03 10:11:19 +0800

    Update dcase24_ntu_teacher_logit_stand_tv1.py

--- dataset/dcase24_ntu_teacher_logit_stand_tv1.py
+++ dataset/dcase24_ntu_teacher_logit_stand_tv1.py
@@ -41,7 +41,8 @@ dataset_config = {
     # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL.pt")      # 3 BCBL 3 SIT
     # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL_augs.pt")      # 3 BCBL 3 SIT all augs sub5_single_SIT
     # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3PaSST_3BCBL_gated.pt")      # 3 BCBL 3 SIT all augs sub5_gated
-    "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL_gated.pt")      # 3 BCBL 3 SIT all augs sub5_gated
+    # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL_gated.pt")      # 3 BCBL 3 SIT all augs sub5_gated
+    "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3SIT_3BCBL_max_vote.pt")      # 3 BCBL 3 SIT all augs sub5_maxvote
     # "logits_file": os.path.join("predictions","ensemble", "sub5_single_SIT.pt")
     # "logits_file": os.path.join("predictions","ensemble", "sub5_ensemble_3PaSST_3BCBL_nomixup.pt")       # NTU ensemble no mixup
     
